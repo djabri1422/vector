9f180a93b38e116d83f1a18ae6d1347327b3abb6
djabri1422 <elmezov99@gmail.com>
2017-03-13 13:44:22 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -6,3 +6,45 @@ SCENARIO("vector init", "[init]") {
 	REQUIRE(vector.size() == 0);
 	REQUIRE(vector.capacity() == 0);
 }
+
+SCENARIO("vector with params", "[init]") {
+	vector_t vector;
+	REQUIRE(vector.size()==3);
+	REQUIRE(vector.capacity() ==3);
+	for(int i=0;i<vector.size();i++){
+		REQUIRE(ptr[i]==0)
+	}
+}
+
+SCENARIO("vector with vector","[init]"){
+	vector_t vector;
+	REQUIRE (vector.ptr==0);
+	size=second.size;
+	capacity=second.capacity;
+	for(int i=0;i<size_;i++)
+		ptr=new int *[size];
+	for(int i=0;i<size_;i++){
+		ptr[i]=second.ptr[i];
+	}
+}
+
+SCENARIO("vector delete","[init]"){
+	vector_t vector;
+	for(int i=0;i<size();i++){
+		delete []ptr;
+	}
+}
+
+SCENARIO ("vector operator = ","[init]"){
+	vector_t vector;
+	for(int i=0;i<size();i++){
+		delete []ptr;
+	}
+	delete[]ptr;
+	ptr=new int*[size];	
+	for(int i=0;i<vector.size();i++){
+
+	}
+
+}
+
